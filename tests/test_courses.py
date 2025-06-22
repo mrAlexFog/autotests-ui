05dbad0514e8048064d9d8d6d14058c9b94f3e85
mrAlexFog <tomonow.a@yandex.ru>
2025-06-22 14:09:14 +0300

    create pytest.ini and conftest.py

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,38 +1,17 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
 
 
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(
+    chromium_page_with_state: Page
+):
+    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    _locator_title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+    expect(_locator_title).to_contain_text("Courses")
 
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
+    _locator_sub_title = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+    expect(_locator_sub_title).to_contain_text("There is no results")
 
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path='browser-state.json')
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-
-        _locator_title = page.get_by_test_id("courses-list-toolbar-title-text")
-        expect(_locator_title).to_contain_text("Courses")
-
-        _locator_sub_title = page.get_by_test_id("courses-list-empty-view-title-text")
-        expect(_locator_sub_title).to_contain_text("There is no results")
